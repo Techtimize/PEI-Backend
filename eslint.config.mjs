161ca43e424788b4fe133ef004e9f5ef9670083e
imanali2237 <imanali341012@gmail.com>
2025-05-15 16:02:20 +0500

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint, { pluginJs } from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+];

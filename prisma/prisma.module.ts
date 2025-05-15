5afac9423432ddf713ec8b98a1f7c8e093ba8650
imanali2237 <imanali341012@gmail.com>
2025-05-15 17:32:10 +0500

    test

--- prisma/prisma.module.ts
+++ prisma/prisma.module.ts
@@ -1,6 +1,5 @@
-import { Global, Module } from '@nestjs/common'
-import { PrismaService } from './prisma.service'
- how are u sa
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 @Global()
 @Module({
   providers: [PrismaService],
